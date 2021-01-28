69c431caedb592c5475c5191c49a520748c43c30
Suhas <suhpin95@gmail.com>
2021-01-28 21:27:32 +0530

    Created Components with default props

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+
+export const Button = ({ text, color, onClick }) => {
+    return (
+        <>
+            <button onClick = {onClick} style= {{ backgroundColor: color }}className="btn"> {text} </button>
+        </>
+    )
+}
+
+Button.defaultProps = {
+    text : 'Add',
+    color : 'green'
+}
+
+Button.prototype = {
+    text : PropTypes.string,
+    color : PropTypes.string,
+    onClick : PropTypes.func
+}
