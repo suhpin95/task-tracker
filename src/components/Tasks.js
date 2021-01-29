86b21e2be1182166fc7070fc7de9609033bf2012
Suhas <suhpin95@gmail.com>
2021-01-29 20:29:23 +0530

    Setting the state in Tasks components

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import { useState } from "react";
 
 const Tasks = () => {
-    const tasks = [ 
+     const [tasks, setTasks] = useState ([ 
         {'id': 1, 'title': 'Title1' , 'description': 'Lorum Impsum'},
         {'id': 2, 'title': 'Title2' , 'description': 'Lorum Impsum'},
         {'id': 3, 'title': 'Title3' , 'description': 'Lorum Impsum'},
-     ]
+     ])
     return (
         <>
             { tasks.map(task=> (
