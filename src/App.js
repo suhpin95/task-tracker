4c9a4489c5079cb1bd5b5ac2dff0883ed8bd83a5
Suhas <suhpin95@gmail.com>
2021-01-30 18:22:14 +0530

    1. Style Enforcement 2. Add Task Implemented 3. Condition renderning of the form for Add Button implemented

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import './App.css';
-import { useState } from "react";
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
+import './App.css'
+import React, { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTasks from './components/AddTasks'
+import { v4 as uuidv4 } from 'uuid'
 
-function App() {
-  const [tasks, setTasks] = useState([ 
-    {'id': 1, 'title': 'Title1' , 'description': 'Lorum Impsum' , 'reminder' : true},
-    {'id': 2, 'title': 'Title2' , 'description': 'Lorum Impsum' , 'reminder' : true},
-    {'id': 3, 'title': 'Title3' , 'description': 'Lorum Impsum' , 'reminder' : false},
- ])
- const deleteTask = (id) => {
-  setTasks(tasks.filter((task)=> task.id !== id));
- }
+function App () {
+  const [showTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    { id: 1, title: 'Title1', description: 'Lorum Impsum', reminder: true },
+    { id: 2, title: 'Title2', description: 'Lorum Impsum', reminder: true },
+    { id: 3, title: 'Title3', description: 'Lorum Impsum', reminder: false }
+  ])
 
- const toggleReminder = (id) => {
-  setTasks(tasks.map( task => task.id === id ? {...task,reminder:!task.reminder} : task) )
- }
+  const addTask = (task) => {
+    const id = uuidv4()
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
   return (
     <div className="container">
-      <Header title = {" Task Tracker "} />
+      <Header onAdd = {() => setShowAddTask(!showTask)} title = {' Task Tracker '} showAdd = { showTask } />
+      { showTask && <AddTasks onAdd = {addTask}/>}
       {
-        tasks.length > 0 ? ( <Tasks tasks = { tasks } onDelete = {deleteTask} onToggle = {toggleReminder}  />) 
-        : 'No Tasks'
+        tasks.length > 0
+          ? (<Tasks tasks = { tasks } onDelete = {deleteTask} onToggle = {toggleReminder} />)
+          : 'No Tasks'
       }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 import PropTypes from 'prop-types'
-import { Button } from "./Button";
-const Header = ( { title } ) => {
-    const onClick = (e) => {
-        console.log("clicked")
-    }
-    return (
+import React from 'react'
+import { Button } from './Button'
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
         <header className = "header">
             <h1>{ title }</h1>
-            <Button onClick = {onClick}/>
+            <Button onClick = {onAdd}
+                color = {showAdd ? 'red' : 'green'}
+                text = { showAdd ? 'Close' : 'Add' }
+            />
         </header>
-    )
+  )
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  onAdd: PropTypes.func,
+  showAdd: PropTypes.func
 }
 
 export default Header
